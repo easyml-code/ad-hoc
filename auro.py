f8bdc014a97ee5d0ca26ccf1b77495fb1e11a611
Marvik-co <75751384+Marvik-co@users.noreply.github.com>
2022-12-06 22:02:31 +0530

    Update auro.py

--- auro.py
+++ auro.py
@@ -1,1 +1,10 @@
-bv
+from collections import defaultdict
+
+class BookStore:
+    # main class to store all the books and their details, which is a dictionary
+    def _init_(self) -> None:
+        self.bookstore = dict()
+    
+    def _addBook(self, bookname, content):
+        self.bookstore[bookname] = content
+    
